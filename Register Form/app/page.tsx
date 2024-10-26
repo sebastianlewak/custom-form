2604b686c976968b9857acdb8dc078fe43c95b5f
Owieczek <112249898+Owieczek@users.noreply.github.com>
2024-10-26 22:16:33 +0200

    feat: title changes

--- Register Form/app/page.tsx
+++ Register Form/app/page.tsx
@@ -115,7 +115,7 @@ export default function Example() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Create an Account
+            Create an Account rt
           </h1>
         </div>
 
